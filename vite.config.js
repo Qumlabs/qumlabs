548a9efbd8b2472836652715d0a3bd4c2d80a338
Qumlabs <qumlabs@gmail.com>
2025-06-19 16:03:47 -0400

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-// const path = require('path')
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // root:path.resolve(__dirname, 'src'),
-  // build:{
-  //   outDir:"../dist"
-  // }
 })
